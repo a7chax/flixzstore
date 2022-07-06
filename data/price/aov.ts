80d46ed7445c9fb443e818c1a8598dc1c64153dd
arungicahaya <akuarung83@gmail.com>
2022-07-07 01:43:38 +0700

    update data price topup

---
+++ data/price/aov.ts
@@ -0,0 +1,168 @@
+import { iItemTopUp } from "../interface";
+
+
+export const ListTopUpAOV : iItemTopUp[] = [
+  {
+    currencyName : "Vouchers",
+    currencyImage : "google.com",
+    amount : "40",
+    price : "10.000",
+  },
+  {
+    currencyName : "Vouchers",
+    currencyImage : "google.com",
+    amount : "90",
+    price : "19.000",
+  },
+  {
+    currencyName : "Vouchers",
+    currencyImage : "google.com",
+    amount : "130",
+    price : "28.000",
+  },
+  {
+    currencyName : "Vouchers",
+    currencyImage : "google.com",
+    amount : "180",
+    price : "37.000",
+  },
+  {
+    currencyName : "Vouchers",
+    currencyImage : "google.com",
+    amount : "230",
+    price : "46.000",
+  },
+  {
+    currencyName : "Vouchers",
+    currencyImage : "google.com",
+    amount : "270",
+    price : "55.000",
+  },
+  {
+    currencyName : "Vouchers",
+    currencyImage : "google.com",
+    amount : "320",
+    price : "63.000",
+  },
+  {
+    currencyName : "Vouchers",
+    currencyImage : "google.com",
+    amount : "360",
+    price : "72.000",
+  },
+  {
+    currencyName : "Vouchers",
+    currencyImage : "google.com",
+    amount : "410",
+    price : "81.000",
+  },
+  {
+    currencyName : "Vouchers",
+    currencyImage : "google.com",
+    amount : "470",
+    price : "90.000",
+  },
+  {
+    currencyName : "Vouchers",
+    currencyImage : "google.com",
+    amount : "510",
+    price : "99.000",
+  },
+  {
+    currencyName : "Vouchers",
+    currencyImage : "google.com",
+    amount : "560",
+    price : "108.000",
+  },
+  {
+    currencyName : "Vouchers",
+    currencyImage : "google.com",
+    amount : "600",
+    price : "117.000",
+  },
+  {
+    currencyName : "Vouchers",
+    currencyImage : "google.com",
+    amount : "650",
+    price : "126.000",
+  },
+  {
+    currencyName : "Vouchers",
+    currencyImage : "google.com",
+    amount : "700",
+    price : "108.000",
+  },
+  {
+    currencyName : "Vouchers",
+    currencyImage : "google.com",
+    amount : "740",
+    price : "144.000",
+  },
+  {
+    currencyName : "Vouchers",
+    currencyImage : "google.com",
+    amount : "790",
+    price : "152.000",
+  },
+  {
+    currencyName : "Vouchers",
+    currencyImage : "google.com",
+    amount : "830",
+    price : "161.000",
+  },
+  {
+    currencyName : "Vouchers",
+    currencyImage : "google.com",
+    amount : "880",
+    price : "170.000",
+  },
+  {
+    currencyName : "Vouchers",
+    currencyImage : "google.com",
+    amount : "950",
+    price : "179.000",
+  },
+  {
+    currencyName : "Vouchers",
+    currencyImage : "google.com",
+    amount : "1180",
+    price : "224.00",
+  },
+  {
+    currencyName : "Vouchers",
+    currencyImage : "google.com",
+    amount : "1420",
+    price : "268.000",
+  },
+  {
+    currencyName : "Vouchers",
+    currencyImage : "google.com",
+    amount : "1650",
+    price : "313.000",
+  },
+  {
+    currencyName : "Vouchers",
+    currencyImage : "google.com",
+    amount : "1900",
+    price : "357.000",
+  },
+  {
+    currencyName : "Vouchers",
+    currencyImage : "google.com",
+    amount : "2850",
+    price : "535.000",
+  },
+  {
+    currencyName : "Vouchers",
+    currencyImage : "google.com",
+    amount : "3800",
+    price : "713.000",
+  },
+  {
+    currencyName : "Vouchers",
+    currencyImage : "google.com",
+    amount : "4750",
+    price : "891.000",
+  },
+
+]
---
+++ data/price/honkaiimpact.ts
@@ -0,0 +1,137 @@
+import { iItemTopUp } from "../interface";
+
+
+export const ListTopUpHonkaiImpact : iItemTopUp[] = [
+  {
+    currencyName : "Monthly Card",
+    currencyImage : "google.com",
+    amount : "",
+    price : "69.000",
+  },
+  {
+    currencyName : "Crystal",
+    currencyImage : "google.com",
+    amount : "130",
+    price : "29.000",
+  },
+  {
+    currencyName : "Crystal",
+    currencyImage : "google.com",
+    amount : "330",
+    price : "69.000",
+  },
+  {
+    currencyName : "Crystal",
+    currencyImage : "google.com",
+    amount : "710",
+    price : "136.000",
+  },
+  {
+    currencyName : "Crystal",
+    currencyImage : "google.com",
+    amount : "1040",
+    price : "203.000",
+  },
+  {
+    currencyName : "Crystal",
+    currencyImage : "google.com",
+    amount : "1430",
+    price : "271.000",
+  },
+  {
+    currencyName : "Crystal",
+    currencyImage : "google.com",
+    amount : "2140",
+    price : "405.000",
+  },
+  {
+    currencyName : "Crystal",
+    currencyImage : "google.com",
+    amount : "2860",
+    price : "540.000",
+  },
+  {
+    currencyName : "Crystal",
+    currencyImage : "google.com",
+    amount : "3860",
+    price : "667.000",
+  },
+  {
+    currencyName : "Crystal",
+    currencyImage : "google.com",
+    amount : "4570",
+    price : "801.000",
+  },
+  {
+    currencyName : "Crystal",
+    currencyImage : "google.com",
+    amount : "5290",
+    price : "936.000",
+  },
+  {
+    currencyName : "Crystal",
+    currencyImage : "google.com",
+    amount : "7720",
+    price : "1.332.000",
+  },
+  {
+    currencyName : "B-Chips",
+    currencyImage : "google.com",
+    amount : "65",
+    price : "15.000",
+  },
+  {
+    currencyName : "B-Chips",
+    currencyImage : "google.com",
+    amount : "130",
+    price : "29.000",
+  },
+  {
+    currencyName : "B-Chips",
+    currencyImage : "google.com",
+    amount : "330",
+    price : "69.000",
+  },
+  {
+    currencyName : "B-Chips",
+    currencyImage : "google.com",
+    amount : "660",
+    price : "137.000",
+  },
+  {
+    currencyName : "B-Chips",
+    currencyImage : "google.com",
+    amount : "990",
+    price : "199.000",
+  },
+  {
+    currencyName : "B-Chips",
+    currencyImage : "google.com",
+    amount : "1320",
+    price : "272.000",
+  },
+  {
+    currencyName : "B-Chips",
+    currencyImage : "google.com",
+    amount : "1980",
+    price : "397.000",
+  },
+  {
+    currencyName : "B-Chips",
+    currencyImage : "google.com",
+    amount : "3300",
+    price : "667.000",
+  },
+  {
+    currencyName : "B-Chips",
+    currencyImage : "google.com",
+    amount : "4620",
+    price : "937.000",
+  },
+  {
+    currencyName : "B-Chips",
+    currencyImage : "google.com",
+    amount : "6660",
+    price : "1.332.000",
+  },
+]
