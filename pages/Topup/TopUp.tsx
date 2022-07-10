44ca29f837a634d59366a210edeae5ec1867776d
arungicahaya <akuarung83@gmail.com>
2022-07-10 21:45:03 +0700

    push custom mesasge

--- pages/Topup/TopUp.tsx
+++ pages/Topup/TopUp.tsx
@@ -14,22 +14,20 @@ import styles from '../../styles/Home.module.css'
 import { dataGame } from "../../data/game";
 import { ListTopUpMobileLegend } from "../../data/price/mlbb";
 import { iItemTopUp } from "../../data/interface";
-import useTopUp from "./useTopUp";
-
 
 const TopUp : NextPage = () => {
 
-
-  const {
-    setSelectedTopup,
-    selectedTopup,
-    setTextInput,
-    textInput,
-    selectTopUpNya,
-    first,
-    linkWa
-  } = useTopUp()
   
+  
+  
+  
+    const [selectedTopup, setSelectedTopup] = useState<iItemTopUp>()
+    const [textInput, setTextInput] = useState<String>()
+  
+  
+    const linkWa = `https://api.whatsapp.com/send?phone=6585706425856&text=Nick+%3A+Arungi+Cahaya%0AID+%3A+Arung%0AServer+%3A+12312312%0AOrder+%3A+${textInput}%0ASS+sisa+diamond+UC+%3A+20`
+  
+
   const data =() => {
     return ListTopUpMobileLegend
   }
